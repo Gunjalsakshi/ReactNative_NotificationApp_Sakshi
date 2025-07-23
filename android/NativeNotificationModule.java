83916bc1bf9a3901e206e513fc8e4dd3c4c7000f
Sakshi Gunjal <117565086+Gunjalsakshi@users.noreply.github.com>
2025-07-23 16:34:03 +0530

    Update NativeNotificationModule.java

--- android/NativeNotificationModule.java
+++ android/NativeNotificationModule.java
@@ -1,3 +1,23 @@
-// NativeNotificationModule.java
-// Placeholder for native Android module
-// Will be implemented in Kotlin/Java once system access is available
+package com.reactnativepush;
+
+import android.widget.Toast;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+
+public class NativeNotificationModule extends ReactContextBaseJavaModule {
+
+    NativeNotificationModule(ReactApplicationContext context) {
+        super(context);
+    }
+
+    @Override
+    public String getName() {
+        return "NativeNotification";
+    }
+
+    @ReactMethod
+    public void showNotification(String message) {
+        Toast.makeText(getReactApplicationContext(), message, Toast.LENGTH_LONG).show();
+    }
+}
